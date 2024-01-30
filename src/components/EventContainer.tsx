0739ffa7512e38e458f05ae730855aeb385c3106
Ibrahim Syed <syedi@oregonstate.edu>
2024-01-29 17:11:39 -0800

    Basic game logic

--- src/components/EventContainer.tsx
+++ src/components/EventContainer.tsx
@@ -7,11 +7,33 @@ const numEvents = 3; // number of events to be displayed at one time
 
 const EventContainer: React.FC = () => {
     const [eventsToShow, setEventsToShow] = useState(numEvents);
+    const [guessedYear, setGuessedYear] = useState<number | null>(null);
+    const [victory, setVictory] = useState(false);
+
+    const minYear = historicalData.minYear;
+    const maxYear = historicalData.maxYear;
 
     // iterate eventsToShow by numEvents
     const loadMoreEvents = () => {
         setEventsToShow((prevEventsToShow) => prevEventsToShow + numEvents);
     };
+
+    // handle year submission & guess checking logic
+    const handleYearSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (guessedYear != null && guessedYear >= minYear && guessedYear <= maxYear) {
+            setVictory(true);
+        } else {
+            setEventsToShow((prevEventsToShow) => prevEventsToShow + numEvents);
+        }
+    }
+
+    // handle input field change
+    const handleYearChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const year = parseInt(event.target.value, 10);
+        setGuessedYear(year);
+    }
 
     return (
         <div>
@@ -19,9 +41,27 @@ const EventContainer: React.FC = () => {
             <EventRow events={historicalData.events.slice(0, eventsToShow)} />
 
             {/* load more events button */}
-            {eventsToShow < historicalData.events.length && (
+            {!victory && eventsToShow < historicalData.events.length && (
                 <LoadMore onClick={loadMoreEvents} />
             )}
+
+            {/* year input & submit button */}
+            {!victory && (
+                <form onSubmit={handleYearSubmit}>
+                    <label>
+                        What year is it?
+                        <input 
+                            type="number"
+                            value={guessedYear !== null ? guessedYear : ''}
+                            onChange={handleYearChange}
+                        />
+                    </label>
+                    <button type="submit">Submit</button>
+                </form>
+            )}
+
+            {/* Victory screen */}
+            {victory && <div>Congratulations! You guessed the correct year!</div>}
         </div>
     );
 };
